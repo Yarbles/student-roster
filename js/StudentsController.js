453e63e4d6005a20404ab185d897dcdfaea789fb
Reid Baldwin & Bryan Borgeson <student@epicodus.com>
2015-04-20 09:56:48 -0700

    Initial commit

---
+++ js/StudentsController.js
@@ -0,0 +1,9 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+    $scope.students = [
+        { name: "Amanda Hugginkiss" },
+        { name: "Ipee Freely" },
+        { name: "Mike Rotch" },
+        { name: "Harry Richard" },
+        { name: "Sr. Ruppert H. Cornelius III" }
+    ]
+});
